91a2ace600ef2a649adb61d258773a52f3640b68
Junior dos santos <juniorbrunasantos2019@gmail.com>
2025-01-30 17:01:31 -0300

    Adicionando todos os arquivos do projeto: controllers, DTOs, entidades, enums, repository, services, etc.

---
+++ PizzaJbApplication.java
@@ -0,0 +1,13 @@
+package com.PizzaJB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaJbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaJbApplication.class, args);
+	}
+
+}
